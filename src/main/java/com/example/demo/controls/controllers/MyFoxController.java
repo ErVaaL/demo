77aed77519c61c105d914e4298bd191fdbd29379
ErVaaL <130446401+ErVaaL@users.noreply.github.com>
2023-12-19 12:31:27 +0100

    Łączenie z api

--- src/main/java/com/example/demo/controls/controllers/MyFoxController.java
+++ src/main/java/com/example/demo/controls/controllers/MyFoxController.java
@@ -23,57 +23,57 @@ public class MyFoxController {
         model.addAttribute("foxes", service.getAllAnimals());
         return "index";
     }
-//    @GetMapping(value = "/error")
-//    public String getErrorMessage(Model model){
-//
-//        return "error";
-//    }
-//
-//    @GetMapping(value = "/addFoxToBase")
-//    public String getFoxView(Model model){
-//        model.addAttribute("fox", new Fox("",0));
-//        return "addFoxToBase";
-//    }
-//    @PostMapping(value = "/addFoxToBase")
-//    public String addFox( Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
-//        if(fox.getTails() < 1){
-//            model.addAttribute("errorMessage", "fox has to have tail");
-//            return "addFoxToBase";
-//        }
-//        service.addAnimal(fox);
-//        redirectAttributes.addFlashAttribute("successMessage", "added successfully");
-//        return "redirect:/index";
-//    }
-//    @GetMapping(value = "/editFox/{id}")
-//    public String editFox(Model model, @PathVariable("id") String id){
-//        var fox = service.getFoxById(Long.parseLong(id));
-//        model.addAttribute("fox", fox);
-//        return "editFox";
-//    }
-//    @PostMapping(value = "/editFox/{id}")
-//    public String editFox(Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
-//        if(fox.getTails() < 1 || fox.getName().trim().equals(null)){
-//            model.addAttribute("errorMessage", "Neither of fields can be null");
-//            return "editFox";
-//        }
-//        service.putAnimal(fox);
-//        redirectAttributes.addFlashAttribute("successMessage", "Fox edited");
-//        return "redirect:/index";
-//    }
-//    @GetMapping(value = "/deleteFox/{id}")
-//    public String deleteFox(Model model, @PathVariable("id") String id){
-//        var fox = service.getFoxById(Long.parseLong(id));
-//        model.addAttribute("fox", fox);
-//        return "deleteFox";
-//    }
-//    @PostMapping(value = "/deleteFox/{id}")
-//    public String deleteFox(Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
-//        if(service.getFoxById(fox.getId()) == null){
-//            model.addAttribute("errorMessage", "No such fox in database");
-//            return "/deleteFox";
-//        }
-//        service.deleteAnimal(fox.getId());
-//        model.addAttribute("successMessage", "Fox has been deleted");
-//        return "redirect:/index";
-//    }
+    @GetMapping(value = "/error")
+    public String getErrorMessage(Model model){
+
+        return "error";
+    }
+
+    @GetMapping(value = "/addFoxToBase")
+    public String getFoxView(Model model){
+        model.addAttribute("fox", new Fox("",0));
+        return "addFoxToBase";
+    }
+    @PostMapping(value = "/addFoxToBase")
+    public String addFox( Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
+        if(fox.getTails() < 1){
+            model.addAttribute("errorMessage", "fox has to have tail");
+            return "addFoxToBase";
+        }
+        service.addAnimal(fox);
+        redirectAttributes.addFlashAttribute("successMessage", "added successfully");
+        return "redirect:/index";
+    }
+    @GetMapping(value = "/editFox/{id}")
+    public String editFox(Model model, @PathVariable("id") String id){
+        var fox = service.getFoxById(Long.parseLong(id));
+        model.addAttribute("fox", fox);
+        return "editFox";
+    }
+    @PostMapping(value = "/editFox/{id}")
+    public String editFox(Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
+        if(fox.getTails() < 1 || fox.getName().trim().equals(null)){
+            model.addAttribute("errorMessage", "Neither of fields can be null");
+            return "editFox";
+        }
+        service.putAnimal(fox);
+        redirectAttributes.addFlashAttribute("successMessage", "Fox edited");
+        return "redirect:/index";
+    }
+    @GetMapping(value = "/deleteFox/{id}")
+    public String deleteFox(Model model, @PathVariable("id") String id){
+        var fox = service.getFoxById(Long.parseLong(id));
+        model.addAttribute("fox", fox);
+        return "deleteFox";
+    }
+    @PostMapping(value = "/deleteFox/{id}")
+    public String deleteFox(Model model, RedirectAttributes redirectAttributes, @ModelAttribute Fox fox){
+        if(service.getFoxById(fox.getId()) == null){
+            model.addAttribute("errorMessage", "No such fox in database");
+            return "/deleteFox";
+        }
+        service.deleteAnimal(fox.getId());
+        model.addAttribute("successMessage", "Fox has been deleted");
+        return "redirect:/index";
+    }
 }
--- src/main/java/com/example/demo/controls/service/MyRestService.java
+++ src/main/java/com/example/demo/controls/service/MyRestService.java
@@ -5,6 +5,7 @@ import com.example.demo.exceptions.FoxFailedToUpdateException;
 import com.example.demo.objects.Fox;
 import com.example.demo.exceptions.FoxNotFoundException;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -26,25 +27,44 @@ public class MyRestService {
     }
 //    public List<Fox> getFoxByTails(int tails){return (List<Fox>) this.repository.findAllByTails(tails);}
 
-//    public Fox getFoxById(Long id){
-//        if(repository.findById(id).isEmpty()) throw new FoxNotFoundException();
-//        return this.repository
-//                .findById(id).orElse(new Fox("default",2));}
+    public Fox getFoxById(Long id){
+       var fox = restClient.get()
+                .uri(API_URL + "/getAnimalById/"+id)
+                .retrieve()
+                .body(new ParameterizedTypeReference<Fox>() {
+                });
+        if(fox == null) throw new FoxNotFoundException();
+        return fox;
+    }
 
-//    public void addAnimal(Fox animal){
-//        if(animal.getId() != null) throw new FoxAlreadyExistsException();
-//        repository.save(animal);
-//    }
+    public void addAnimal(Fox animal){
+        if(animal.getId() != null) throw new FoxAlreadyExistsException();
+        restClient.post()
+                .uri(API_URL + "/addAnimal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(animal)
+                .retrieve()
+                .toBodilessEntity();
+    }
     
-//    public void putAnimal(Fox animal){
-//        if(!repository.existsById(animal.getId())) throw new FoxFailedToUpdateException();
-//        repository.save(animal);
-//    }
+    public void putAnimal(Fox animal){
+        if(this.getFoxById(animal.getId()) == null) throw new FoxFailedToUpdateException();
+        restClient.put()
+                .uri(API_URL + "/updateAnimal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(animal)
+                .retrieve()
+                .toBodilessEntity();
+    }
 
-//    public void deleteAnimal(Long id){
-//        if(repository.findById(id).isPresent())this.repository.deleteById(id);
-//        else throw new FoxNotFoundException();
-//    }
+    public void deleteAnimal(Long id){
+        var fox = this.getFoxById(id);
+        if(this.getFoxById(id) != null ){
+         restClient.delete();
+
+        }
+        else throw new FoxNotFoundException();
+    }
 
 //    public List<Fox> filterFoxByName(String name) {
 //        var foxes = (List<Fox>)this.repository.findAll();
