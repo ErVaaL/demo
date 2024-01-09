5754011eba8e1221dbcf54ae6c9ab6263ea90a5c
ErVaaL <130446401+ErVaaL@users.noreply.github.com>
2024-01-09 12:14:48 +0100

    Dodanie testów updatów i deletów

--- demo-backend/src/test/java/com/application/test/MyRestAssuredControllerTest.java
+++ demo-backend/src/test/java/com/application/test/MyRestAssuredControllerTest.java
@@ -14,20 +14,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MyRestAssuredControllerTest {
     private static final String URI = "http://localhost:8080";
 
-//    @BeforeAll
-//    public void setUp(){
-//        with()
-//                .body(new Fox("testFox",5))
-//                .contentType("application/json")
-//                .port(URI + "/addAnimal");
-//    }
-//    @AfterAll
-//    public void tearDown(){
-//        with()
-//                .body(new Fox("testFox",5))
-//                .contentType("application/json")
-//                .delete(URI + "/deleteAnimal");
-//    }
+    @BeforeAll
+    public static void setUp(){
+        Fox testFox = new Fox("testFox1",3);
+        testFox.setId(5L);
+        with()
+                .body(testFox)
+                .contentType("application/json")
+                .post(URI + "/addAnimal");
+    }
+    @AfterAll
+    public static void tearDown(){
+        with()
+                .delete(URI + "/deleteAnimal/5");
+    }
 
     @Test
     public void testGetFox(){
@@ -53,6 +53,52 @@ public class MyRestAssuredControllerTest {
                 .body("name", equalTo("testFox"))
                 .body("tails",equalTo(5))
                 .statusCode(200);
-
+    }
+    @Test
+    public void testFoxAddingWhenExists(){
+        Fox fox = new Fox("Kokos",1);
+        fox.setId(1L);
+        with()
+                .body(fox)
+                .contentType("application/json")
+                .post(URI + "/addAnimal")
+                .then()
+                .statusCode(400);
+    }
+    @Test
+    public void testFoxEditing(){
+        Fox fox = new Fox("testFox", 7);
+        fox.setId(1L);
+        with()
+                .body(fox)
+                .contentType("application/json")
+                .put(URI+"/updateAnimal")
+                .then()
+                .statusCode(200);
+    }
+    @Test
+    public void testFoxEditingFoxUpdateFail(){
+        Fox fox = new Fox("testFox", 7);
+        fox.setId(21312L);
+        with()
+                .body(fox)
+                .contentType("application/json")
+                .put(URI+"/updateAnimal")
+                .then()
+                .statusCode(400);
+    }
+    @Test
+    public void testFoxDeleting(){
+        when()
+                .delete(URI + "/deleteAnimal/4")
+                .then()
+                .statusCode(200);
+    }
+    @Test
+    public void testFoxDeletingNotFound(){
+        when()
+                .delete(URI + "/deleteAnimal/2022")
+                .then()
+                .statusCode(404);
     }
 }
